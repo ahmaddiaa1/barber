20fa2e362f9b2197c8c1ea2bb0214cc80e6dfcff
Unknown <ahmad.diaa1972@gmail.com>
2025-01-16 21:24:35 +0200

    add validate pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 config();
 async function bootstrap() {
@@ -11,6 +12,9 @@ async function bootstrap() {
     credentials: true,
   });
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ forbidUnknownValues: false, whitelist: true }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

13693cd79ae35bc03d168321edf4407838af2004
Unknown <ahmad.diaa1972@gmail.com>
2025-01-16 21:03:31 +0200

    download class-validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
+  signup(createAuthDto: User) {
     return 'This action adds a new auth';
   }
-
-  findAll() {
-    return `This action returns all auth`;
+  login(createAuthDto: User) {
+    return 'This action adds a new auth';
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  logout(createAuthDto: User) {
+    return 'This action adds a new auth';
   }
 }

c57fd6a1493e942d619ed9f55c780e6b7a303a02
kareemkhaled <123971567+kareemkhaledd1@users.noreply.github.com>
2025-01-17 22:33:48 +0200

    Improve AppSuccess constructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,6 +34,7 @@ export class AuthService {
     });
     return new AppSuccess(newUser, 'user created successfully', 201);
   }
+
   async login(createAuthDto: LoginDto) {
     const { phone, password } = createAuthDto;
 
@@ -54,6 +55,7 @@ export class AuthService {
       statusCode: 201,
     };
   }
+
   async logout() {}
 
   verifyToken(token: string) {

2228cd26bb2bbff7a378b3e1d9896119e495a8dd
Unknown <ahmad.diaa1972@gmail.com>
2025-01-17 23:42:44 +0200

    adding role guard

--- decorators/roles.decoretor.ts
+++ decorators/roles.decoretor.ts
@@ -1,3 +1,4 @@
-import { Reflector } from '@nestjs/core';
+import { SetMetadata } from '@nestjs/common';
+import { Role } from '@prisma/client';
 
-export const Roles = Reflector.createDecorator<string[]>();
+export const Roles = (roles: Role[]) => SetMetadata('roles', roles);
--- guard/role.guard.ts
+++ guard/role.guard.ts
@@ -1,18 +1,38 @@
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { Reflector } from '@nestjs/core';
-// import { Roles } from 'decorators/roles.decoretor';
+// roles.guard.ts
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
 
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//   constructor(private reflector: Reflector) {}
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
 
-//   canActivate(context: ExecutionContext): boolean {
-//     const roles = this.reflector.get(Roles, context.getHandler());
-//     if (!roles) {
-//       return true;
-//     }
-//     const request = context.switchToHttp().getRequest();
-//     const user = request.user;
-//     return matchRoles(roles, user.roles);
-//   }
-// }
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true; // No roles specified, allow access
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user; // Assume user is already set in request (via JWT, for example)
+
+    if (!user) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    const hasRole = requiredRoles.some((role) => user.roles?.includes(role));
+    if (!hasRole) {
+      throw new ForbiddenException('Insufficient roles');
+    }
+
+    return true;
+  }
+}

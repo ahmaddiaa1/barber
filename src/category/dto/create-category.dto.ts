d632d8fe84725315aa6b737c4a177a49cf1d69e8
kareemkhaled <123971567+kareemkhaledd1@users.noreply.github.com>
2025-03-04 22:09:22 +0200

    Remove name property from CreateCategoryDto and retain available property

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,10 +1,6 @@
 import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
-  @IsNotEmpty()
-  @IsString()
-  name: string;
-
   @IsBoolean()
   @IsOptional()
   available: boolean;

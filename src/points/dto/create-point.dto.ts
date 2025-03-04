c62b341503a7005c355034e3df97358a5cdf5ace
Diaa <125160987+ahmaddiaa1@users.noreply.github.com>
2025-03-05 00:20:50 +0200

    Add transformation to price and points in CreatePointDto for numeric conversion

--- src/points/dto/create-point.dto.ts
+++ src/points/dto/create-point.dto.ts
@@ -1,13 +1,16 @@
 import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { translationDto } from '../../../src/class-type/translation';
+import { Transform } from 'class-transformer';
 
 export class CreatePointDto {
   @IsNotEmpty()
   @IsNumber()
+  @Transform(({ value }) => +value)
   price: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @Transform(({ value }) => +value)
   points: number;
 
   @IsArray()

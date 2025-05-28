abf308dcfc6d32320f0ce858bec0ad4c38bb99d3
Diaa <125160987+ahmaddiaa1@users.noreply.github.com>
2025-05-29 02:31:49 +0300

    fix: Improve Firebase private key handling and update FCM token response format

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -14,7 +14,7 @@ export class NotificationService {
   private secret = process.env.JWT_SECRET;
 
   async setFCMToken(user: User, fcmToken: string) {
-    await this.prisma.user.update({
+    const token = await this.prisma.user.update({
       where: {
         id: user.id,
       },
@@ -23,7 +23,7 @@ export class NotificationService {
       },
     });
 
-    return { message: 'FCM token set' };
+    return new AppSuccess(token.fcmToken, 'FCM token updated successfully');
   }
 
   async getNotification(user: User) {

67120b57a6a2e17888e0e164da2ae9a9cb5f35fc
Diaa <125160987+ahmaddiaa1@users.noreply.github.com>
2025-06-11 17:53:37 +0300

    feat: Update SMS sender name and enhance verification code handling

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -16,16 +16,15 @@ export class SmsService {
   private readonly logger = new Logger(SmsService.name);
 
   constructor(private readonly prisma: PrismaService) {
+    this.senderName = process.env.SMS_SENDERNAME;
     this.username = process.env.SMS_USERNAME;
     this.password = process.env.SMS_PASSWORD;
-    this.senderName = process.env.SMS_SENDER_NAME;
   }
 
   async sendVerificationCode(mobile: string) {
-    const code = 12356; // This should be generated dynamically, e.g., using a random number generator
-
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
     const message = `Your verification code is ${code}`;
-    const url = `${process.env.SMS_API_URL}?username=${this.username}&password=${this.password}&sendername=${this.senderName}&message=${message}&mobiles=${mobile}`;
+    const url = `https://smssmartegypt.com/sms/api/`;
 
     console.log(url);
 
@@ -36,15 +35,50 @@ export class SmsService {
           Accept: 'application/json',
           'Accept-Language': 'en-US',
         },
+        params: {
+          username: this.username,
+          password: this.password,
+          sendername: this.senderName,
+          message: message,
+          mobiles: mobile,
+        },
       });
 
+      // console.log(data.type ?? data[0].type);
       console.log(data);
-
-      return data;
+      const type = data.type ?? data[0].type;
+      if (type === 'error' && data.error.number === 300) {
+        await this.prisma.phoneVerification.create({
+          data: {
+            code: code.toString(),
+            phone: mobile.toString(),
+          },
+        });
+        return { data: true };
+      } else if (type === 'error' && data.error.number !== 300) {
+        this.logger.error(`SMS API Error: ${data.error.message}`);
+        return { data: false };
+      }
+      return { data: true, code };
     } catch (e) {
       const error = e as Error;
       this.logger.error(`Error sending SMS: ${error.message}`, error.stack);
-      throw new InternalServerErrorException(error);
+      return { data: false };
     }
   }
+
+  // async verifyCode(mobile: string, code: string) {
+  //   const sms = await this.prisma.sms.findFirst({
+  //     where: { mobile, code },
+  //   });
+
+  //   if (!sms) {
+  //     throw new InternalServerErrorException('Invalid verification code');
+  //   }
+
+  //   // Optionally, you can delete the SMS record after successful verification
+  //   await this.prisma.sms.delete({ where: { id: sms.id } });
+
+  //   return { message: 'Verification successful' };
+  // }
 }

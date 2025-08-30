27a24d757fc7dce3bdb146a855ea82d8f1877ae2
Diaa <125160987+ahmaddiaa1@users.noreply.github.com>
2025-08-30 12:11:12 +0300

    fix(auth): add logging for referral code validation during user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,6 +67,8 @@ export class AuthService {
         break;
 
       case Role.USER:
+        console.log(code, referralCodeStatus);
+        console.log(createAuthDto);
         if (code && !referralCodeStatus) {
           throw new BadRequestException('Referral code is invalid');
         }
